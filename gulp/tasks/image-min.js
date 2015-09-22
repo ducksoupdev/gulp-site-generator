b2db09c2714d341528954e435c1f19bd42f18ad8
levym <ducksoupdev@gmail.com>
2015-09-22 20:52:24 +0100

    Replace imagemin-jpegoptim with imagemin-mozjpeg.
    * Uplift node modules

--- gulp/tasks/image-min.js
+++ gulp/tasks/image-min.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var gulp = require('gulp'),
-        jpegoptim = require('imagemin-jpegoptim'),
+        imageminMozjpeg = require('imagemin-mozjpeg'),
         pngquant = require('imagemin-pngquant'),
         optipng = require('imagemin-optipng');
 
@@ -10,7 +10,7 @@
         return gulp.src('./src/images/**/*.{png,jpg,jpeg,gif,svg}')
             .pipe(optipng({optimizationLevel: 3})())
             .pipe(pngquant({quality: '65-80', speed: 4})())
-            .pipe(jpegoptim({max: 70})())
+            .pipe(imageminMozjpeg({quality: 70})())
             .pipe(gulp.dest('./build/images'));
     });
 })();
