285fa1d43ceef55b45839468037c0307b40d698a
Larry Williamson <lawrencealan@gmail.com>
2016-11-10 13:37:58 -0500

    Fix livereload race condition + HBS watch
    livereload once for multiple changes (within 100ms), reload AFTER build directory changes, watch hbs templates and rebuild, write file changes to console

--- gulp/tasks/develop.js
+++ gulp/tasks/develop.js
@@ -1,7 +1,8 @@
 "use strict";
 
 var gulp = require("gulp"),
-    connect = require("gulp-connect");
+    connect = require("gulp-connect"),
+    gutil = require('gulp-util');
 
 gulp.task("livereload-connect", ["build"], function () {
     connect.server({
@@ -15,11 +16,20 @@ gulp.task("livereload-html", function () {
         .pipe(connect.reload());
 });
 
+var _lto = 0;
 gulp.task("livereload-watch", function () {
-    gulp.watch(["./src/sass/**/*.scss"], ["build", "livereload-html"]);
-    gulp.watch(["./src/js/**/*.js"], ["build", "livereload-html"]);
-    gulp.watch(["./src/images/**/*.{gif,jpg,png}"], ["build", "livereload-html"]);
-    gulp.watch(["./src/content/**/*.md"], ["build", "livereload-html"]);
+    gulp.watch(["./src/sass/**/*.scss"], ["build"]);
+    gulp.watch(["./src/templates/**/*.hbs"], ["build"]);
+    gulp.watch(["./src/js/**/*.js"], ["build"]);
+    gulp.watch(["./src/images/**/*.{gif,jpg,png}"], ["build"]);
+    gulp.watch(["./src/content/**/*.md"], ["build"]);
+    gulp.watch(["./build/**/*.*"]).on('change', function (event) {
+        gutil.log(gutil.colors.green('-'),event.path.replace(process.cwd(),''), gutil.colors.magenta(event.type));
+        clearTimeout(_lto);
+        _lto = setTimeout(function () {
+            gulp.start("livereload-html");
+        }, 100);
+    });
 });
 
 gulp.task("develop", ["livereload-connect", "livereload-watch"]);
