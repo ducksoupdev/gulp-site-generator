2fbc26d37e320cc60719526c4fd93e38bc6257c8
Alex Rodriguez <drinkhorchata@gmail.com>
2017-08-10 18:16:45 -0500

    add browsersync and remove uncss

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,10 +1,17 @@
 "use strict";
 
 var gulp = require("gulp"),
-    sass = require("gulp-sass");
+    sass = require("gulp-sass"),
+    minifyCSS = require("gulp-minify-css"),
+    rename = require("gulp-rename");
 
 gulp.task("sass", function () {
     return gulp.src("./src/sass/**/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("./src/css"));
+        .pipe(gulp.dest("./src/css"))
+        .pipe(minifyCSS())
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest("./build/css"));
 });
