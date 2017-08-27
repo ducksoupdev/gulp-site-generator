422d5688ab9fb10354e0c35f2576452c2e1bd15b
alex rodriguez <alex@horchatadesign.com>
2017-08-27 01:16:59 -0500

    add browser-sync and remove uncss (#35)
    * add browsersync and remove uncss

--- gulp/tasks/develop.js
+++ gulp/tasks/develop.js
@@ -1,37 +1,37 @@
-"use strict";
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
 
-var gulp = require("gulp"),
-    connect = require("gulp-connect"),
-    open = require("gulp-open"),
-    gutil = require("gulp-util");
-
-gulp.task("livereload-connect", ["build"], function () {
-    connect.server({
-        root: "./build",
-        livereload: true
-    });
-    gulp.src(__filename).pipe(open({uri: "http://localhost:8080"}));
+/* Reload task */
+gulp.task('bs-reload', () => {
+  browserSync.reload();
 });
 
-gulp.task("livereload-html", function () {
-    gulp.src("./build")
-        .pipe(connect.reload());
+/* Prepare Browser-sync for localhost */
+gulp.task('browser-sync', () => {
+  browserSync.init(['css/*.css', 'js/*.js'], {
+    server: {
+      baseDir: './'
+    }      
+  });
 });
 
-var _lto = 0;
-gulp.task("livereload-watch", function () {
-    gulp.watch(["./src/sass/**/*.scss"], ["uncss"]);
+gulp.task('serve', () => {
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    }
+  });
+});
+
+
+gulp.task('watch', () => {
+    gulp.watch(["./src/sass/**/*.scss"], ["sass"]);
     gulp.watch(["./src/templates/**/*.hbs"], ["minify-html"]);
     gulp.watch(["./src/js/**/*.js"], ["concat-js"]);
     gulp.watch(["./src/images/**/*.{gif,jpg,png}"], ["image-min"]);
     gulp.watch(["./src/content/**/*.md"], ["minify-html"]);
-    gulp.watch(["./build/**/*.*"]).on("change", function (event) {
-        gutil.log(gutil.colors.green("-"),event.path.replace(process.cwd(),""), gutil.colors.magenta(event.type));
-        clearTimeout(_lto);
-        _lto = setTimeout(function () {
-            gulp.start("livereload-html");
-        }, 100);
-    });
-});
+    gulp.watch(["./build/**/*.*"], ["bs-reload"]);
+})
 
-gulp.task("develop", ["livereload-connect", "livereload-watch"]);
+gulp.task("develop", ["watch", "serve"]);
